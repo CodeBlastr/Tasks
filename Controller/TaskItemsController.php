488b13afff2435105c06dea2a86fc831d699dff8
Buildrr LLC <richard@buildrr.com>
2014-01-20 05:48:20 +0000

    Added a new controller, to start the process of cleaning up all the confusion between task lists and task items.

---
+++ Controller/TaskItemsController.php
@@ -0,0 +1,36 @@
+<?php
+class AppTaskItemsController extends TasksAppController { 
+
+	public $name = 'TaskItems';
+
+	public $allowedActions = array('edit'); // remove this
+
+/**
+ * @todo This should be TaskChild at some point.  More comments 
+ * on that in the Task Model. 
+ */
+	public $uses = array(
+		'Tasks.Task',
+	);
+
+/**
+ * Edit method
+ */
+	public function edit($id = null) {
+		if ($this->request->is('post')) {
+			if ($this->Task->save($this->request->data)) {
+				$this->Session->setFlash(__('The Task has been saved'));
+				// redirect to the parent
+				$id = !empty($this->request->data['Task']['parent_id']) ? $this->request->data['Task']['parent_id'] : $this->Task->field('parent_id', array('Task.id' => $this->Task->id));
+				$this->redirect(array('controller' => 'tasks', 'action' => 'view', $id));
+			} else {
+				$this->Session->setFlash(__('The Task could not be saved. Please, try again.'));
+			}
+		}
+	}
+	
+}
+
+if (!isset($refuseInit)) {
+		class TaskItemsController extends AppTaskItemsController {}
+}
